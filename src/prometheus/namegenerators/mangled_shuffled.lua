0848946d95d470a76bf78168209809e02ee9bb8d
oprty131 <slwmplylw@gmail.com>
2026-07-15 21:53:11 +0300

    Enhance name generation with Unicode digits
    Added Unicode digits to name generation and shuffled them.

--- src/prometheus/namegenerators/mangled_shuffled.lua
+++ src/prometheus/namegenerators/mangled_shuffled.lua
@@ -5,11 +5,12 @@
 -- This Script provides a function for generation of mangled names with shuffled character order
 
 
-local util = require("prometheus.util");
-local chararray = util.chararray;
+local util = require("prometheus.util")
+local chararray = util.chararray
 
-local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_");
-local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+local UnicodeDigits = {"І", "Ӏ", "ⅼ", "ǀ", "ℓ", "𝟙", "𝟏", "1̶", "𐌠", "Ⅰ", "ǁ", "ǂ", "ǃ", "ǀ"}
 
 local function generateName(id, _)
 	local name = ''
@@ -19,17 +20,22 @@ local function generateName(id, _)
 	while id > 0 do
 		local e = id % #VarDigits
 		id = (id - e) / #VarDigits
-		name = name..VarDigits[e+1]
+		if math.random() > 0.8 then
+			name = name..UnicodeDigits[math.random(#UnicodeDigits)]
+		else
+			name = name..VarDigits[e+1]
+		end
 	end
 	return name
 end
 
 local function prepare(_)
-	util.shuffle(VarDigits);
-	util.shuffle(VarStartDigits);
+	util.shuffle(VarDigits)
+	util.shuffle(VarStartDigits)
+	util.shuffle(UnicodeDigits)
 end
 
 return {
 	generateName = generateName,
 	prepare = prepare
-};
+}
