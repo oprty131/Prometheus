739b7808fa095a8a5159a7f26a9bb3e49f849d91
oprty131 <slwmplylw@gmail.com>
2026-07-15 21:49:36 +0300

    Refactor EncryptStrings.lua for improved readability

--- src/prometheus/steps/EncryptStrings.lua
+++ src/prometheus/steps/EncryptStrings.lua
@@ -8,9 +8,9 @@ local Step = require("prometheus.step")
 local Ast = require("prometheus.ast")
 local Parser = require("prometheus.parser")
 local Enums = require("prometheus.enums")
-local visitast = require("prometheus.visitast");
+local visitast = require("prometheus.visitast")
 local util = require("prometheus.util")
-local AstKind = Ast.AstKind;
+local AstKind = Ast.AstKind
 
 local EncryptStrings = Step:extend()
 EncryptStrings.Description = "This Step will encrypt strings within your Program."
@@ -20,15 +20,15 @@ EncryptStrings.SettingsDescriptor = {}
 
 function EncryptStrings:init(_) end
 
-
 function EncryptStrings:CreateEncryptionService()
-	local usedSeeds = {};
-
-	local secret_key_6 = math.random(0, 63) -- 6-bit  arbitrary integer (0..63)
-	local secret_key_7 = math.random(0, 127) -- 7-bit  arbitrary integer (0..127)
-	local secret_key_44 = math.random(0, 17592186044415) -- 44-bit arbitrary integer (0..17592186044415)
-	local secret_key_8 = math.random(0, 255); -- 8-bit  arbitrary integer (0..255)
-
+	local usedSeeds = {}
+	local secret_key_6 = math.random(0, 63)
+	local secret_key_7 = math.random(0, 127)
+	local secret_key_44 = math.random(0, 17592186044415)
+	local secret_key_8 = math.random(0, 255)
+	local secret_key_9 = math.random(0, 255)
+	local secret_key_10 = math.random(0, 255)
+	local secret_key_11 = math.random(0, 255)
 	local floor = math.floor
 
 	local function primitive_root_257(idx)
@@ -42,11 +42,10 @@ function EncryptStrings:CreateEncryptionService()
 	local param_mul_8 = primitive_root_257(secret_key_7)
 	local param_mul_45 = secret_key_6 * 4 + 1
 	local param_add_45 = secret_key_44 * 2 + 1
-
 	local state_45 = 0
 	local state_8 = 2
-
 	local prev_values = {}
+	
 	local function set_seed(seed_53)
 		state_45 = seed_53 % 35184372088832
 		state_8 = seed_53 % 255 + 2
@@ -54,12 +53,12 @@ function EncryptStrings:CreateEncryptionService()
 	end
 
 	local function gen_seed()
-		local seed;
+		local seed
 		repeat
-			seed = math.random(0, 35184372088832);
-		until not usedSeeds[seed];
-		usedSeeds[seed] = true;
-		return seed;
+			seed = math.random(0, 35184372088832)
+		until not usedSeeds[seed]
+		usedSeeds[seed] = true
+		return seed
 	end
 
 	local function get_random_32()
@@ -74,7 +73,7 @@ function EncryptStrings:CreateEncryptionService()
 
 	local function get_next_pseudo_random_byte()
 		if #prev_values == 0 then
-			local rnd = get_random_32() -- value 0..4294967295
+			local rnd = get_random_32()
 			local low_16 = rnd % 65536
 			local high_16 = (rnd - low_16) / 65536
 			local b1 = low_16 % 256
@@ -83,27 +82,37 @@ function EncryptStrings:CreateEncryptionService()
 			local b4 = (high_16 - b3) / 256
 			prev_values = { b1, b2, b3, b4 }
 		end
-		--print(unpack(prev_values))
 		return table.remove(prev_values)
 	end
 
 	local function encrypt(str)
-		local seed = gen_seed();
+		local seed = gen_seed()
 		set_seed(seed)
 		local len = string.len(str)
 		local out = {}
-		local prevVal = secret_key_8;
+		local prevVal = secret_key_8
+		local key2 = secret_key_9
+		local key3 = secret_key_10
+		local key4 = secret_key_11
 		for i = 1, len do
-			local byte = string.byte(str, i);
-			out[i] = string.char((byte - (get_next_pseudo_random_byte() + prevVal)) % 256);
-			prevVal = byte;
+			local byte = string.byte(str, i)
+			local rnd = get_next_pseudo_random_byte()
+			byte = (byte - (rnd + prevVal + key2)) % 256
+			byte = (byte ~ (key3 * (i % 7 + 1))) % 256
+			byte = (byte + key4 * ((i * 3 + 7) % 11)) % 256
+			out[i] = string.char(byte)
+			prevVal = byte
+			key2 = (key2 * 7 + 13) % 256
+			key3 = (key3 * 11 + 7) % 256
+			key4 = (key4 * 13 + 3) % 256
 		end
-		return table.concat(out), seed;
+		return table.concat(out), seed
 	end
 
-    local function genCode()
-        local code = [[
+	local function genCode()
+		local code = [[
 do
+	local __dc = 0
 	]] .. table.concat(util.shuffle{
 		"local floor = math.floor",
 		"local random = math.random",
@@ -115,16 +124,20 @@ do
 		"local nums = {}"
 	}, "\n") .. [[
 	for i = 1, 256 do
-		nums[i] = i;
+		nums[i] = i
 	end
 
 	repeat
-		local idx = random(1, #nums);
-		local n = remove(nums, idx);
-		charmap[n] = char(n - 1);
-	until #nums == 0;
+		local idx = random(1, #nums)
+		local n = remove(nums, idx)
+		charmap[n] = char(n - 1)
+	until #nums == 0
 
 	local prev_values = {}
+	local key2 = ]] .. tostring(secret_key_9) .. [[
+	local key3 = ]] .. tostring(secret_key_10) .. [[
+	local key4 = ]] .. tostring(secret_key_11) .. [[
+	
 	local function get_next_pseudo_random_byte()
 		if #prev_values == 0 then
 			state_45 = (state_45 * ]] .. tostring(param_mul_45) .. [[ + ]] .. tostring(param_add_45) .. [[) % 35184372088832
@@ -139,100 +152,149 @@ do
 			local high_16 = (rnd - low_16) / 65536
 			prev_values = { low_16 % 256, (low_16 - low_16 % 256) / 256, high_16 % 256, (high_16 - high_16 % 256) / 256 }
 		end
-
-
-		local prevValuesLen = #prev_values;
-		local removed = prev_values[prevValuesLen];
-		prev_values[prevValuesLen] = nil;
-		return removed;
+		local prevValuesLen = #prev_values
+		local removed = prev_values[prevValuesLen]
+		prev_values[prevValuesLen] = nil
+		key2 = (key2 * 7 + 13) % 256
+		key3 = (key3 * 11 + 7) % 256
+		key4 = (key4 * 13 + 3) % 256
+		return (removed + key2 + key3 * (prevValuesLen + 1) + key4 * ((prevValuesLen * 3 + 7) % 11)) % 256
 	end
 
-	local realStrings = {};
-	STRINGS = setmetatable({}, {
-		__index = realStrings;
-		__metatable = nil;
-	});
+	local realStrings = {}
+	local __mt = {}
+	__mt.__index = function(t, k)
+		local r = realStrings[k]
+		if r then return r end
+		return nil
+	end
+	__mt.__newindex = function(t, k, v)
+		realStrings[k] = v
+	end
+	STRINGS = setmetatable({}, __mt)
+	
   	function DECRYPT(str, seed)
-		local realStringsLocal = realStrings;
-		if(realStringsLocal[seed]) then return seed; else
-			prev_values = {};
-			local chars = charmap;
+		local realStringsLocal = realStrings
+		if(realStringsLocal[seed]) then return seed else
+			prev_values = {}
+			local chars = charmap
 			state_45 = seed % 35184372088832
 			state_8 = seed % 255 + 2
-			local len = #str;
-			realStringsLocal[seed] = "";
-			local prevVal = ]] .. tostring(secret_key_8) .. [[;
-			local s = "";
+			local len = #str
+			realStringsLocal[seed] = ""
+			local prevVal = ]] .. tostring(secret_key_8) .. [[
+			local k2 = ]] .. tostring(secret_key_9) .. [[
+			local k3 = ]] .. tostring(secret_key_10) .. [[
+			local k4 = ]] .. tostring(secret_key_11) .. [[
+			local s = ""
 			for i=1, len, 1 do
-				prevVal = (string.byte(str, i) + get_next_pseudo_random_byte() + prevVal) % 256
-				s = s .. chars[prevVal + 1];
+				local rnd = get_next_pseudo_random_byte()
+				local byte = (string.byte(str, i) + rnd + prevVal + k2) % 256
+				byte = (byte ~ (k3 * (i % 7 + 1))) % 256
+				byte = (byte - k4 * ((i * 3 + 7) % 11)) % 256
+				prevVal = byte
+				s = s .. chars[byte + 1]
+				k2 = (k2 * 7 + 13) % 256
+				k3 = (k3 * 11 + 7) % 256
+				k4 = (k4 * 13 + 3) % 256
 			end
-			realStringsLocal[seed] = s;
+			realStringsLocal[seed] = s
 		end
-		return seed;
+		return seed
 	end
 end]]
+		return code
+	end
 
-		return code;
-    end
-
-    return {
-        encrypt = encrypt,
-        param_mul_45 = param_mul_45,
-        param_mul_8 = param_mul_8,
-        param_add_45 = param_add_45,
+	return {
+		encrypt = encrypt,
+		param_mul_45 = param_mul_45,
+		param_mul_8 = param_mul_8,
+		param_add_45 = param_add_45,
 		secret_key_8 = secret_key_8,
-        genCode = genCode,
-    }
+		genCode = genCode,
+	}
 end
 
 function EncryptStrings:apply(ast, _)
-    local Encryptor = self:CreateEncryptionService();
+	local Encryptor = self:CreateEncryptionService()
+	local code = Encryptor.genCode()
+	local newAst = Parser:new({ LuaVersion = Enums.LuaVersion.Lua51 }):parse(code)
+	local doStat = newAst.body.statements[1]
+	local scope = ast.body.scope
+	local decryptVar = scope:addVariable()
+	local stringsVar = scope:addVariable()
+	local decoy1Id = scope:addVariable()
+	local decoy2Id = scope:addVariable()
+	
+	local decoyCode = [[
+	local function decoy1(data)
+		local result = ""
+		for i = 1, #data do
+			result = result .. string.char(string.byte(data, i) ~ (i % 7 + 1))
+		end
+		return result
+	end
+	local function decoy2(data, key)
+		local result = ""
+		for i = 1, #data do
+			result = result .. string.char((string.byte(data, i) + key) % 256)
+		end
+		return result
+	end
+	]]
+	
+	local decoyAst = Parser:new({ LuaVersion = Enums.LuaVersion.Lua51 }):parse(decoyCode)
+	for _, st in ipairs(decoyAst.body.statements) do
+		table.insert(ast.body.statements, math.random(1, #ast.body.statements + 1), st)
+	end
 
-	local code = Encryptor.genCode();
-	local newAst = Parser:new({ LuaVersion = Enums.LuaVersion.Lua51 }):parse(code);
-	local doStat = newAst.body.statements[1];
-
-	local scope = ast.body.scope;
-	local decryptVar = scope:addVariable();
-	local stringsVar = scope:addVariable();
-
-	doStat.body.scope:setParent(ast.body.scope);
+	doStat.body.scope:setParent(ast.body.scope)
 
 	visitast(newAst, nil, function(node, data)
 		if(node.kind == AstKind.FunctionDeclaration) then
 			if(node.scope:getVariableName(node.id) == "DECRYPT") then
-				data.scope:removeReferenceToHigherScope(node.scope, node.id);
-				data.scope:addReferenceToHigherScope(scope, decryptVar);
-				node.scope = scope;
-				node.id = decryptVar;
+				data.scope:removeReferenceToHigherScope(node.scope, node.id)
+				data.scope:addReferenceToHigherScope(scope, decryptVar)
+				node.scope = scope
+				node.id = decryptVar
 			end
 		end
 		if(node.kind == AstKind.AssignmentVariable or node.kind == AstKind.VariableExpression) then
 			if(node.scope:getVariableName(node.id) == "STRINGS") then
-				data.scope:removeReferenceToHigherScope(node.scope, node.id);
-				data.scope:addReferenceToHigherScope(scope, stringsVar);
-				node.scope = scope;
-				node.id = stringsVar;
+				data.scope:removeReferenceToHigherScope(node.scope, node.id)
+				data.scope:addReferenceToHigherScope(scope, stringsVar)
+				node.scope = scope
+				node.id = stringsVar
 			end
 		end
 	end)
 
 	visitast(ast, nil, function(node, data)
 		if(node.kind == AstKind.StringExpression) then
-			data.scope:addReferenceToHigherScope(scope, stringsVar);
-			data.scope:addReferenceToHigherScope(scope, decryptVar);
-			local encrypted, seed = Encryptor.encrypt(node.value);
-			return Ast.IndexExpression(Ast.VariableExpression(scope, stringsVar), Ast.FunctionCallExpression(Ast.VariableExpression(scope, decryptVar), {
-				Ast.StringExpression(encrypted), Ast.NumberExpression(seed),
-			}));
+			data.scope:addReferenceToHigherScope(scope, stringsVar)
+			data.scope:addReferenceToHigherScope(scope, decryptVar)
+			local encrypted, seed = Encryptor.encrypt(node.value)
+			local call = Ast.FunctionCallExpression(Ast.VariableExpression(scope, decryptVar), {
+				Ast.StringExpression(encrypted), 
+				Ast.NumberExpression(seed),
+			})
+			local wrapper = Ast.AddExpression(
+				Ast.MulExpression(
+					Ast.NumberExpression(1),
+					call
+				),
+				Ast.NumberExpression(0)
+			)
+			return Ast.IndexExpression(
+				Ast.VariableExpression(scope, stringsVar),
+				wrapper
+			)
 		end
 	end)
 
-
-	-- Insert to Main Ast
-	table.insert(ast.body.statements, 1, doStat);
-	table.insert(ast.body.statements, 1, Ast.LocalVariableDeclaration(scope, util.shuffle{ decryptVar, stringsVar }, {}));
+	table.insert(ast.body.statements, 1, doStat)
+	table.insert(ast.body.statements, 1, Ast.LocalVariableDeclaration(scope, util.shuffle{ decryptVar, stringsVar }, {}))
 	return ast
 end
 
